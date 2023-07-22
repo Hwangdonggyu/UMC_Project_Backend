dbb4ee1a61640a0ff36440b1fcd12581c3bc0ef0
황동규 <hdg2342@naver.com>
2023-07-22 15:32:20 +0900

    DB 스키마 upload

---
+++ models/Diary.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const diarySchema = mongoose.Schema({
+	writer: {
+		type: mongoose.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	content: {
+		type: String,
+		maxLength: 1000,
+		required: true,
+	},
+	imgUrl: [
+		{
+			type: String,
+		},
+	],
+	created_at: {
+		type: Date,
+		default: Date.now,
+		required: true,
+	},
+});
+
+const Diary = mongoose.model("Diary", diarySchema);
+
+module.exports = Diary;
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+	{
+		// username:     {type: String, required: true},
+		kakaoId: { type: String, required: true, unique: true },
+		nickname: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true },
+		password: { type: String, required: true },
+		phone: { type: String, required: true, unique: true },
+		age: { type: Number, required: true },
+		bloodType: { type: String, required: true },
+		imageUrl: { type: String },
+		started_date: { type: Date, required: true },
+		birthday: { type: Date, required: true },
+		connectCode: { type: String, required: true, unique: true }, // 커플 연결 코드
+		partnerId: { type: String, required: true, unique: true },
+		createdAt: { type: Date, default: Date.now, required: true },
+		//  created_date: {type: Date  , required: true},
+		//  modified_date:{type: Date  , required: true},
+	},
+	{ timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
