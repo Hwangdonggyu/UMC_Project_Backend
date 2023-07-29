63182a7fd3392c8ea317a246952985c7ee4a5a06
황동규 <hdg2342@naver.com>
2023-07-29 16:01:17 +0900

    user 뼈대 upload

---
+++ src/models/users/dto/index.js
@@ -0,0 +1,10 @@
+const {UsersDTO} = require("./users.dto");
+const {UpdateUserDTO} = require("./update-user.dto");
+const {CreateUserDTO} = require("./create-user.dto");
+
+module.exports = {
+  UsersDTO,
+  UpdateUserDTO,
+  CreateUserDTO
+};
+
