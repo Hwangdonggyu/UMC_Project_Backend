f9a8efb0297ab38588ee05dc31ef61e743611c9b
Yeaeun411 <yeaeun411@naver.com>
2023-08-03 12:47:05 +0900

    Add GetMethod on bannedWordRouter

--- routers/bannedWordRouter.js
+++ routers/bannedWordRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bannedWordController = require("./controllers/bannedWordController");
 
 router.post('/', bannedWordController.saveBannedWord);
+router.get('/:id', bannedWordController.getBannedWord);
 router.put('/:id', bannedWordController.updateBannedWord);
 router.delete('/:id', bannedWordController.deleteBannedWord);
 
