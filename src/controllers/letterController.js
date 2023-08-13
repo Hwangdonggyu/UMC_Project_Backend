b2634a20e20d0f71b0bb7e583cf11f7300cfa071
Park Dongkyu <qkrehdrb0813@gmail.com>
2023-08-13 22:24:57 +0900

    src 폴더 추가

---
+++ src/controllers/letterController.js
@@ -0,0 +1,106 @@
+const Letter = require("../models/Letter");
+const User = require("../models/User");
+
+// 보낸 편지함 내역 보기
+exports.getSentLetters = async (req, res) => {
+	// 로그인 된 회원의 _id 가져옴
+	const {
+		session: {
+			user: { _id },
+		},
+	} = req;
+
+	// 그 유저의 보낸 편지함 populate
+	const user = await User.findById(_id).populate("sentLetters");
+
+	return res.status(200).json(user.sentLetters); // 최신순으로 정렬.
+};
+
+// 받은 편지함 내역 보기
+exports.getReceivedLetters = async (req, res) => {
+	// 로그인 된 회원의 _id 가져옴
+	const {
+		session: {
+			user: { _id },
+		},
+	} = req;
+
+	// 그 유저의 받은 편지함 populate
+	const user = await User.findById(_id).populate("receivedLetters");
+
+	return res.status(200).json(user.receivedLetters); // 최신순으로 정렬.
+};
+
+// 편지 보내기 화면 보기.
+exports.getSendLetter = async (req, res) => {
+	return res.render("sendLetter.pug");
+};
+
+// 편지 전송 기능.
+exports.postSendLetter = async (req, res) => {
+	const {
+		session: {
+			user: { _id, partnerId },
+		},
+
+		// 편지 내용 가져오기.
+		body: {
+			reasonForReconciliation,
+			reasonForOccurrence,
+			currentFeelings,
+			waysToMaintainRelationship,
+			apologyWithLove,
+			letter,
+		}, // 클라이언트에서 보내주는 letter의 구조를 안 후에 작성하도록.
+	} = req;
+
+	// 파트너가 없으면 에러.
+	if (!partnerId) {
+		return res.status(400).json({ message: "파트너가 없습니다." });
+	}
+
+	try {
+		// 편지 객체 생성.
+		const newLetter = await Letter.create({
+			reasonForReconciliation,
+			reasonForOccurrence,
+			currentFeelings,
+			waysToMaintainRelationship,
+			apologyWithLove,
+			letter,
+			from: _id,
+			to: partnerId,
+		});
+
+		const user = await User.findById(_id);
+		user.sentLetters.push(newLetter._id);
+		user.save();
+
+		const partner = await User.findById(partnerId);
+		partner.receivedLetters.push(newLetter._id);
+		partner.save();
+
+		return res
+			.status(200)
+			.json({ message: "편지가 성공적으로 전송되었습니다." });
+	} catch (err) {
+		return res.status(400).json({ message: err });
+	}
+};
+
+// 보낸 편지 보기.
+exports.getLetter = async (req, res) => {
+	const {
+		params: { id },
+	} = req;
+
+	const letter = await Letter.findById(id);
+	// 편지 없음
+	if (!letter) return res.status(400).json({ message: "편지가 없습니다." });
+
+	// 편지가 있으면 읽은 횟수 +1
+	letter.readedCount += 1;
+	letter.save();
+
+	return res.status(200).json(letter);
+};
---
+++ src/server.js
@@ -0,0 +1,37 @@
+require("./db");
+const express = require("express");
+const session = require("express-session"); // express-session 미들웨어를 가져와 사용자 세션을 관리합니다.
+const MongoStore = require("connect-mongo"); // MongoDB에 세션 데이터를 저장하기 위해 connect-mongo를 가져옵니다.
+const morgan = require("morgan"); //  Express용 로깅 미들웨어인 morgan 가져오기.
+
+// Routers
+const userRouter = require("./routers/userRouter.js");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.set("view engine", "pug");
+app.set("views", "src/views");
+
+app.use(express.json()); // 클라이언트가 본문에 JSON 데이터가 포함된 HTTP POST 또는 PUT 요청을 보낼 때(예: axios, fetch 또는 기타 도구를 통해) 이 미들웨어는 JSON 데이터 읽기, 구문 분석 및 결과 첨부를 처리합니다. JavaScript 개체를 request.body 속성에 추가합니다.
+app.use(express.urlencoded({ extended: true })); // 요청 본문에서 들어오는 URL 인코딩 데이터를 구문 분석합니다.
+
+// 세션 미들웨어 설정: express-session 미들웨어를 사용하려면 먼저 앱에 세션 미들웨어를 추가해야 합니다.
+app.use(
+	// session middleware 를추가. router 앞에 설정해야함!
+	session({
+		secret: process.env.COOKIE_SECRET,
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			maxAge: 24 * 60 * 60 * 1000,
+		}, // 24 hours 쿠키 유지시간
+		MongoStore: MongoStore.create({
+			mongoUrl: process.env.DB_URL,
+		}),
+	})
+);
+
+app.use("/letters", userRouter);
+
+module.exports = app;
