c7a6fdbabfd881adb35f895d25b8176f72d45cb5
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2023-08-13 23:05:24 +0900

    profileRouter
    profileRouter

--- src/routers/profileRouter.js
+++ src/routers/profileRouter.js
@@ -1,18 +1,13 @@
 const express = require("express");
 
-const { profilePage, pweditPage, passwordEdit, deleteUser } = require("../controllers/profileController");
+const { passwordEdit, deleteUser } = require("../controllers/profileController");
 const { protectorMiddleware, publicOnlyMiddleware } = require("../middlewares");
 
 const profileRouter = express.Router();
 
 profileRouter
-	.route('/')
-	.all(protectorMiddleware)
-	.get(profilePage);
-profileRouter
 	.route('/pwedit')
 	.all(protectorMiddleware)
-	.get(pweditPage)
 	.put(passwordEdit);
 profileRouter
 	.route('/delete')
