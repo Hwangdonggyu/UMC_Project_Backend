69452bf729d24481278d7e817d02fcf66be9ab5b
Park Dongkyu <qkrehdrb0813@gmail.com>
2023-08-24 21:05:48 +0900

    .

--- src/routers/loginRouter.js
+++ src/routers/loginRouter.js
@@ -1,38 +1,31 @@
 const express = require("express");
-const upload = require('../config/uploadConfig'); // 이미지 업로드 설정 경로 변경
+const upload = require("../config/uploadConfig"); // 이미지 업로드 설정 경로 변경
 
-const { userLogin, userLogout, userRegister, passwordFind, connectWithPartner, sendSupportEmail } = require("../controllers/loginController");
+const {
+	userLogin,
+	userLogout,
+	userRegister,
+	passwordFind,
+	connectWithPartner,
+	sendSupportEmail,
+} = require("../controllers/loginController");
 const { protectorMiddleware, publicOnlyMiddleware } = require("../middlewares");
 
 const userRouter = express.Router();
 
+userRouter.route("/login").all(publicOnlyMiddleware).post(userLogin);
+
 userRouter
-	.route('/login')
+	.route("/register")
 	.all(publicOnlyMiddleware)
-	.post(userLogin);
+	.post(upload.single("profileImage"), userRegister);
 
-userRouter
-	.route('/register')
-	.all(publicOnlyMiddleware)
-	.post(upload.single('profileImage'), userRegister);
+userRouter.route("/findpw").all(publicOnlyMiddleware).post(passwordFind);
 
-userRouter
-	.route('/findpw')
-	.all(publicOnlyMiddleware)
-	.post(passwordFind);
+userRouter.route("/logout").get(userLogout);
 
-userRouter
-	.route('/logout')
-	.get(userLogout);
+userRouter.route("/connect").all(protectorMiddleware).post(connectWithPartner);
 
-userRouter
-	.route('/connect')
-	.all(protectorMiddleware)
-	.post(connectWithPartner);
+userRouter.route("/question").all(protectorMiddleware).get(sendSupportEmail);
 
-userRouter
-	.route('/question')
-	.all(protectorMiddleware)
-	.get(sendSupportEmail);
-	
 module.exports = userRouter;
